d3109713923eed1019c4bd97796d95b8d811cf2d
austinejike <austinejike1@gmail.com>
2021-03-29 19:15:02 +0100

    Update score.js

--- score.js
+++ score.js
@@ -7,19 +7,19 @@ var result
 function gradeCalc(socoreConvert, totalScoreConvert){
     
     if(scoreConvert <= 59){
-               result =  scoreConvert /totalScoreConvert;
+               result =  scoreConvert / 100 * totalScoreConvert ;
                 console.log("F - Your grade percentage is " + " " + result + "%" )
             }else if(socoreConvert >= 60 && socoreConvert <= 69 ) {
-                        result = socoreConvert / totalScoreConvert;
+                        result = socoreConvert / 100 * totalScoreConvert ;
                         console.log("D - Your grade percentage is " + " " + result + "%" )
                     }else if(socoreConvert >= 70 && socoreConvert <= 79 ) {
-                        result = socoreConvert / totalScoreConvert;
+                        result = socoreConvert / 100 * totalScoreConvert;
                         console.log("C - Your grade percentage is " + " " + result + "%" )
                     }else if(socoreConvert >= 80 && socoreConvert<= 89 ) {
-                        result = socoreConvert / totalScoreConvert;
+                        result = socoreConvert / 100  *totalScoreConvert;
                         console.log("B - Your grade percentage is " + " " + result + "%" )
                     }else {
-                                result =  ((socoreConvert / totalScoreConvert) * (totalScoreConvert / 1));
+                                result = socoreConvert / 100 * totalScoreConvert ;
                                 console.log("A - Your grade percentage is " + " " + result + "%" )
                             }
     
