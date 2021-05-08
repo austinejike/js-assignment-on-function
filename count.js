bee2207c40667d56a85886564bf985a768f9e142
austinejike <austinejike1@gmail.com>
2021-05-08 10:58:36 +0100

    done

--- count.js
+++ count.js
@@ -1,12 +1,16 @@
-//function countdown(num ){
-//    for(countdown = num; countdown >=0; countdown --){
-//        if (countdown === 0){
-//            console.log("Blast Off!");
+
+function countdown(string){
+  let num = prompt('Enter number to be conuntdoun')
+let num1 = parseFloat(num)
+    for(countdown = num1; countdown >=0; countdown --){
+        if (countdown === 0){
+            console.log("Blast Off!");
             
-//        }else{
-//            console.log(countdown);
-//        }
-//    }
+        }else{
+            console.log(countdown);
+        }
+    }
          
-// }
-// countdown(5)
+ }
+ countdown()
+
